d9222690fd19a3008a5f07fb9f7beb845d24ea2a
yangzhi <y19961006>
2020-09-10 20:39:55 +0800

    添加注释

--- aet/src/main/java/com/west/aet/file/FileCopy.java
+++ aet/src/main/java/com/west/aet/file/FileCopy.java
@@ -11,9 +11,10 @@ import org.apache.commons.io.filefilter.TrueFileFilter;
 @Getter
 public class FileCopy {
 	
-	// 源目录,或源文件
+	// 源目录,或源文件,填写最顶级的父目录，代码中会去递归查询
 	private String sourceDir = "D:\\eclipse_workspace\\shxi\\library";
-	// 目标目录
+
+	// 目标目录，最终会将源目录下的所有jar复制到此目录的下一级,所有jar包就会在同一级
 	private String targetDir = "D:\\eclipse_workspace\\shxi\\library\\all_jar_bycode";
 	
 	public static void main(String[] args) {
